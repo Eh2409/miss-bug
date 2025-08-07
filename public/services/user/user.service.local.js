5b501e26d010691c3b6fe62eaf2ff4d733e533ee
eliad <eliad.haim@gmail.com>
2025-08-07 08:53:49 +0300

    Add auth functions to user service

--- public/services/user/user.service.local.js
+++ public/services/user/user.service.local.js
@@ -1,6 +1,7 @@
 import { utilService } from '../util.service.js'
 import { storageService } from '../async-storage.service.js'
 
+
 const STORAGE_KEY = 'user'
 
 _createUsers()
@@ -12,7 +13,16 @@ export const userService = {
     add,
     update,
     remove,
+
+    // auth 
+
+    signup,
+    login,
+    logout,
+    getLoggedinUser,
 }
+
+
 
 function query(filterBy = {}) {
     return storageService.query(STORAGE_KEY).then(users => {
@@ -29,6 +39,7 @@ function query(filterBy = {}) {
 function getById(userId) {
     return storageService.get(STORAGE_KEY, userId)
 }
+
 function getByUsername(username) {
     return storageService.query(STORAGE_KEY).then(users => {
         const user = users.find(user => user.username === username)
@@ -85,11 +96,13 @@ function update(updatedUser) {
 
             return storageService.put(STORAGE_KEY, userToSave).then(user => {
                 delete user.password
-                return user
+
+                return _setLoginToken(user)
             })
         })
     })
 }
+
 
 function _createUsers() {
     let users = utilService.loadFromStorage(STORAGE_KEY)
@@ -139,4 +152,75 @@ function _createUsers() {
 
     ]
     utilService.saveToStorage(STORAGE_KEY, users)
+}
+
+
+/// auth functions
+
+const LOGGEDIN_USER_KEY = 'loggedin user'
+
+
+function signup(credentials) {
+    const { username, password, fullname } = credentials
+
+    if (!username || !password || !fullname) {
+        return Promise.reject('Missing required credentials')
+    }
+
+    return userService.add(credentials).then(account => {
+        if (account) {
+            console.log(`account created for user ${account.username}`)
+
+            return login({ username, password })
+
+        } else {
+            return Promise.reject()
+        }
+    })
+
+}
+
+function login({ username, password }) {
+
+    if (!username || !password) {
+        return Promise.reject('Missing required credentials')
+    }
+
+    return userService.getByUsername(username).then(user => {
+
+        if (!user) {
+            return Promise.reject('username or password is incorrect')
+        }
+
+        if (user.password !== password) {
+            return Promise.reject('username or password is incorrect')
+        }
+
+        delete user.password
+
+        return _setLoginToken(user)
+    })
+}
+
+function logout() {
+    sessionStorage.removeItem(LOGGEDIN_USER_KEY)
+    return Promise.resolve()
+}
+
+export function _setLoginToken(user) {
+    user = {
+        _id: user._id,
+        username: user.username,
+        password: user.password,
+        fullname: user.fullname,
+        isAdmin: user.isAdmin
+    }
+    sessionStorage.setItem(LOGGEDIN_USER_KEY, JSON.stringify(user))
+    return user
+}
+
+function getLoggedinUser() {
+    const json = sessionStorage.getItem(LOGGEDIN_USER_KEY)
+    const loggedinUser = JSON.parse(json)
+    return loggedinUser
 }
